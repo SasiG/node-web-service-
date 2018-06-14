9d584749c159c583910d48cc4c2382b79836d646
SasiG <sasidharg3@gmail.com>
2018-06-13 21:35:17 -0500

    Updated app

--- server.js
+++ server.js
@@ -2,10 +2,17 @@ const express = require('express');
 const hbs = require('hbs');
 
 var app = express();
-
+hbs.registerPartials(__dirname+'/views/partials')
 app.set('view engine', 'hbs')
 app.use(express.static(__dirname +'/public'));
 
+hbs.registerHelper('getCurrentYear', () => {
+	return new Date().getFullYear()
+});
+
+hbs.registerHelper('streanIn', (text) =>{
+return text.toUpperCase()
+})
 app.get('/', (req, res) =>{
  res.render('home.hbs',{
  	pageTitle: 'Home Page',
