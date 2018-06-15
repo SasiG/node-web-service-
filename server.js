c16f8d8ec598ae62f9847d9b4bc4d0ca5418be80
SasiG <sasidharg3@gmail.com>
2018-06-14 20:33:20 -0500

    Added logs

--- server.js
+++ server.js
@@ -1,9 +1,27 @@
 const express = require('express');
 const hbs = require('hbs');
+const fs = require('fs');
 
 var app = express();
 hbs.registerPartials(__dirname+'/views/partials')
 app.set('view engine', 'hbs')
+
+app.use((req, res, next) =>{
+	var now = new Date().toString();
+	var log = `${now} ${req.method} ${req.url}`;
+	console.log(log);
+	fs.appendFile('server.log', log +'\n', (err) =>{
+		if(err){
+			console.log('Unable to append the Server')
+		}
+	})
+	next();
+});
+
+app.use((req, res, next) =>{
+	res.render('maintain.hbs')
+})
+
 app.use(express.static(__dirname +'/public'));
 
 hbs.registerHelper('getCurrentYear', () => {
@@ -38,5 +56,3 @@ res.send({
 app.listen(3000, () =>{
 	console.log('App statred on port 3000');
 }) ;
-
-console.log('Looking for a new branch')
