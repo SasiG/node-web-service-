0f57120bcf1e28af2c19302d66d28f7015f64224
SasiG <sasidharg3@gmail.com>
2018-06-17 13:12:47 -0500

    Port

--- server.js
+++ server.js
@@ -1,11 +1,39 @@
 const express = require('express');
 const hbs = require('hbs');
+const fs = require('fs');
+
+var port = process.env.PORT || 3000;
 
 var app = express();
 
+hbs.registerPartials(__dirname+'/views/partials')
 app.set('view engine', 'hbs')
+
+app.use((req, res, next) =>{
+	var now = new Date().toString();
+	var log = `${now} ${req.method} ${req.url}`;
+	console.log(log);
+	fs.appendFile('server.log', log +'\n', (err) =>{
+		if(err){
+			console.log('Unable to append the Server')
+		}
+	})
+	next();
+});
+
+// app.use((req, res, next) =>{
+// 	res.render('maintain.hbs')
+// })
+
 app.use(express.static(__dirname +'/public'));
 
+hbs.registerHelper('getCurrentYear', () => {
+	return new Date().getFullYear()
+});
+
+hbs.registerHelper('streanIn', (text) =>{
+return text.toUpperCase()
+})
 app.get('/', (req, res) =>{
  res.render('home.hbs',{
  	pageTitle: 'Home Page',
@@ -28,6 +56,6 @@ res.send({
 });
 });
 
-app.listen(3000, () =>{
-	console.log('App statred on port 3000');
+app.listen(port, () =>{
+	console.log(`App started on port ${port}`);
 }) ;
